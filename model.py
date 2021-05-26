a753e376cfc00f1fe6c107209490fa5063fc6c1c
Thapelo <81316912+ThapeloMakhalanyane@users.noreply.github.com>
2021-05-26 23:15:10 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -26,6 +26,36 @@ import numpy as np
 import pandas as pd
 import pickle
 import json
+#########################################################
+import seaborn as sns
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+# Preprocessing
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+# Regression models
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import Lasso
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import VotingRegressor
+from sklearn.ensemble import AdaBoostRegressor
+
+# Model slection
+from sklearn.model_selection import train_test_split
+
+# Metrics
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+
+# Other
+from scipy import stats
+import math
+import pickle
+#########################################################
 
 def _preprocess_data(data):
     """Private helper function to preprocess data for model prediction.
@@ -48,7 +78,8 @@ def _preprocess_data(data):
     # Convert the json string to a python dictionary object
     feature_vector_dict = json.loads(data)
     # Load the dictionary as a Pandas DataFrame.
-    feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
+    #feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
+    train_df = pd.DataFrame.from_dict([feature_vector_dict])
 
     # ---------------------------------------------------------------
     # NOTE: You will need to swap the lines below for your own data
@@ -60,9 +91,35 @@ def _preprocess_data(data):
 
     # ----------- Replace this code with your own preprocessing steps --------
     
-
-    feature_vector_df = feature_vector_df[(feature_vector_df['Commodities'] == 'APPLE GOLDEN DELICIOUS')]
-    predict_vector = feature_vector_df[['Total_Qty_Sold','Stock_On_Hand']]
+    train_df = train_df[(train_df['Commodities'] == 'APPLE GOLDEN DELICIOUS')]
+    
+    #Changing date for train
+    train_df['Date']= pd.to_datetime(train_df['Date'])
+    train_df['Months'] = train_df['Date'].dt.strftime('%B')
+    train_df['Year'] = train_df['Date'].dt.strftime('%Y')
+    
+    #Date
+    train_df['year'] = pd.DatetimeIndex(train_df['Date']).year
+    train_df['Months'] = pd.DatetimeIndex(train_df['Date']).month
+    
+    #Changing date for train
+    train_df['Date']= pd.to_datetime(train_df['Date'])
+    train_df['Date'] = train_df['Date'].dt.strftime('%B')
+    
+    #Avarage must be the last column (dependent)
+    #reorder columns
+    column_titles = [col for col in train_df.columns if col!= 'avg_price_per_kg'] + ['avg_price_per_kg']
+    train_df=train_df.reindex(columns=column_titles)
+    
+    #spliting variables into x and y
+    predict_vector = train_df[['Sales_Total','Low_Price','High_Price','Stock_On_Hand','Weight_Kg']]
+    Y = train_df['avg_price_per_kg']
+    
+    
+    
+    
+    #------feature_vector_df = feature_vector_df[(feature_vector_df['Commodities'] == 'APPLE GOLDEN DELICIOUS')]
+    #------predict_vector = feature_vector_df[['Total_Qty_Sold','Stock_On_Hand']]
                                 
     # ------------------------------------------------------------------------
 
